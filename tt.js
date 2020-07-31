48f462256a4cd0bd35795c562dcf1d9c0188d3b7
elvaworkingstation <44248034+elvaworkingstation@users.noreply.github.com>
2020-07-30 21:54:11 -0700

    Update tt.js

--- tt.js
+++ tt.js
@@ -699,3 +699,69 @@ var inorderSuccessor = function(root, p) {
     return successor
   }
 };
+var uniquePaths = function(m, n) {
+    let dp = new Array(m).fill(0).map(x => Array(n).fill(0));
+    for(let i = 0; i < m; i++){
+      dp[i][0] = 1;
+    }
+    for(let j = 0; j < n; j++){
+      dp[0][j] = 1
+    }
+    for(let i = 1; i < m; i++){
+      for(let j =1; j < n; j++){
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+    }
+    console.log(dp);
+    return dp[m - 1][n - 1];
+};
+
+
+
+// Unique Paths II
+
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    if(!obstacleGrid.length || obstacleGrid[0][0] == 1) return 0;
+    let r = obstacleGrid.length;
+    let c = obstacleGrid[0].length;
+    obstacleGrid[0][0] = 1;
+    for(let i = 1; i < r; i++){
+      // obstacleGrid[i][0] = (obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1) ? 1 : 0;
+      if(obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1){
+        obstacleGrid[i][0] = 1;
+      } else obstacleGrid[i][0] = 0;
+    }
+    for(let j = 1; j < c; j++){
+      if(obstacleGrid[0][j] == 0 && obstacleGrid[0][j - 1]){
+        obstacleGrid[0][j] = 1;
+      } else obstacleGrid[0][j] = 0;
+    }
+    for(let i = 1; i < r; i++){
+      for(let j = 1; j < c; j++){
+        if(obstacleGrid[i][j] == 0){
+          obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+        } else {obstacleGrid[i][j] = 0;}
+      }
+    }
+    console.log(obstacleGrid);
+    return obstacleGrid[r - 1][c - 1];
+};
+  
+
+
+//  Minimum Path Sum
+
+var minPathSum = function(grid) {
+    let m = grid.length, n = grid[0].length;
+    for(let i = 1; i < m; ++i) grid[i][0] += grid[i - 1][0];
+    for(let j = 1; j < n; ++j) grid[0][j] += grid[0][j - 1];
+    console.log(grid);
+
+    for(let i = 1; i < m; ++i){
+        for(let j = 1; j < n; ++j){
+            grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+        }
+    }
+    return grid[m-1][n-1];
+};
+
